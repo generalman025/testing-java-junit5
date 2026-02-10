adfb21c7f266d83b2d1220cf8ede8b2f82bd84f9
generalman025 <bass.kiattisak@gmail.com>
2026-02-10 16:41:42 +0700

    assertEquals, assertNotEquals and assertAll lessons

---
+++ src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
@@ -0,0 +1,31 @@
+package guru.springframework.sfgpetclinic.controllers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IndexControllerTest {
+
+    IndexController indexController;
+
+    @BeforeEach
+    void setUp() {
+        indexController = new IndexController();
+    }
+
+    @Test
+    void index() {
+        assertEquals("index", indexController.index());
+        assertEquals("index", indexController.index(), "Wrong View Returned");
+        assertEquals("index", indexController.index(), () -> "Another Expensive Message " +
+                "Make me only if you have to");
+    }
+
+    @Test
+    void oupsHandler() {
+        assertTrue("notimplemented".equals(indexController.oupsHandler()), () -> "This is some expensive"
+        + "Message to build" +
+                "for my test");
+    }
+}
